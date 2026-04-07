e98673846171a094dc8fd1718bb4eee6eece1f38
sweetpotatobikes <nguyenhuevi97@gmail.com>
2026-04-07 15:44:48 -0700

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 == 1) return false;
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') stack.push(c);
+            else {
+                if (stack.isEmpty()) return false;
+                
+                char opening = stack.pop();
+                if (c == ')' && opening != '(') return false;
+                if (c == '}' && opening != '{') return false;
+                if (c == ']' && opening != '[') return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
