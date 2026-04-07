042a3842e42a0eb7d0e0829e2a850c967184318a
sweetpotatobikes <nguyenhuevi97@gmail.com>
2026-04-07 12:22:27 -0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        char[] arr = s.toLowerCase().toCharArray();
+
+        while (left < right) {
+            if (!Character.isLetterOrDigit(arr[left])) {
+                left++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit(arr[right])) {
+                right--;
+                continue;
+            }
+            else if (arr[left] != arr[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
