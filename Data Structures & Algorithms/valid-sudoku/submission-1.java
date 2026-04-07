47b8a1c63872b85e213e230489c0ae96d112baf7
sweetpotatobikes <nguyenhuevi97@gmail.com>
2026-04-07 13:10:24 -0700

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+
+        for (int row = 0; row < board.length; ++row) {
+            for (int col = 0; col < board[0].length; ++col) {
+                char curr = board[row][col];
+                if (curr != '.') {
+                    if (!seen.add(curr + " row " + row) ||
+                        !seen.add(curr + " col " + col) ||
+                        !seen.add(curr + " block " + row/3 + col/3)) {
+                        return false;
+                    } 
+                }
+            }
+        }
+        return true;
+    }
+}
